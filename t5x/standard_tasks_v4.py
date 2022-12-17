b5abc2f84a1bb1909133b683f74be99c5bf3c46b
peterwestai2 <72761013+peterwestai2@users.noreply.github.com>
2022-12-17 00:07:42 -0700

    Update standard_tasks_v4.py

--- t5x/standard_tasks_v4.py
+++ t5x/standard_tasks_v4.py
@@ -1,7 +1,15 @@
+'''
+
+Backup of the updates I made to the new standard tasks, while I try to fix whatever broke it
+
+'''
+
+
 from atomic_seqio_utils import key_prefixes, fill_missing_fields, mask_each_field, combine_inputs
 
 from t5.data import preprocessors
 import t5
+from t5.evaluation import metrics
 import seqio
 import functools
 import tensorflow as tf
@@ -44,6 +52,16 @@ vocabulary = t5.data.get_default_vocabulary()
 fields = list(fields_to_sentinels.keys())
 
 
+DEFAULT_OUTPUT_FEATURES_FINAL = {
+    "inputs":
+        seqio.Feature(
+            vocabulary=vocabulary, add_eos=True),
+    "targets":
+        seqio.Feature(
+            vocabulary=vocabulary, add_eos=True),
+    
+}
+
 DEFAULT_OUTPUT_FEATURES = {
     "inputs":
         seqio.Feature(
@@ -63,7 +81,32 @@ for field in fields:
                                         vocabulary=vocabulary, add_eos=False),})
 
 
-def build_task(input_files, task_name, tsv_fields, mask_fields=None, p_full=0.5):
+    
+    
+def my_tokenize(dataset: tf.data.Dataset) -> tf.data.Dataset:
+  """Encode output features with specified vocabularies.
+  Passes through other features unchanged. Optionally passes through copy
+  of original features with "_pretokenized" suffix added to the key.
+  When `with_eos` is True and input features are ranked > 1, then an EOS is
+  appended only to the last item of each 1-D sequence.
+  Args:
+    dataset: a tf.data.Dataset of examples to tokenize.
+    output_features: a dict of Feature objects; their vocabulary attribute will
+      be used to tokenize the specified features.
+    copy_pretokenized: bool, whether to pass through copies of original features
+      with "_pretokenized" suffix added to the key.
+    with_eos: bool, whether to append EOS to the end of the sequence.
+  Returns:
+    a tf.data.Dataset
+  """
+  tokenize_fn = functools.partial(
+      seqio.preprocessors.tokenize_impl,
+      output_features=DEFAULT_OUTPUT_FEATURES,
+      copy_pretokenized=False,
+      with_eos=False)
+  return seqio.utils.map_over_dataset(fn=tokenize_fn)(dataset)
+    
+def build_task(input_files, task_name, tsv_fields, mask_fields=None, p_full=0.5, field_mask_options = None, metric_fns = []):
     
 
     # if no option given, allow masking of all fields
@@ -75,7 +118,11 @@ def build_task(input_files, task_name, tsv_fields, mask_fields=None, p_full=0.5)
     # i.e. don't mask any fields not in mask_fields
     # also, require at least one mask
     mask_field_inds = [fields.index(field) for field in mask_fields]
-    field_mask_options =  [list(l) for l in itertools.product([0, 1], repeat=len(fields)) if (sum([l[ind] for ind in mask_field_inds]) == sum(l)) and sum(l) > 0]
+    
+    if field_mask_options is None:
+        field_mask_options =  [list(l) for l in itertools.product([0, 1], repeat=len(fields)) if (sum([l[ind] for ind in mask_field_inds]) == sum(l)) and sum(l) > 0]
+
+    
 
     
     # define the task
@@ -93,7 +140,8 @@ def build_task(input_files, task_name, tsv_fields, mask_fields=None, p_full=0.5)
         functools.partial(key_prefixes, fields=fields),
     
         # tokenize 
-        seqio.preprocessors.tokenize,
+        my_tokenize,
+
         
         # use random masking to generate inputs/targets for each field (similar to T5 objective)
         functools.partial(mask_each_field, 
@@ -108,9 +156,14 @@ def build_task(input_files, task_name, tsv_fields, mask_fields=None, p_full=0.5)
         # remove some extraneous features
         functools.partial(
             preprocessors.rekey, key_map={key:key for key in DEFAULT_OUTPUT_FEATURES.keys()}),
+        # remove some extraneous features
+        #functools.partial(
+        #    preprocessors.rekey, key_map={key:key for key in ['inputs','targets']}),
 
     ],
-    output_features=DEFAULT_OUTPUT_FEATURES)
+    metric_fns=metric_fns,
+    #output_features=DEFAULT_OUTPUT_FEATURES)
+    output_features=DEFAULT_OUTPUT_FEATURES_FINAL)
     
     
     
@@ -135,7 +188,7 @@ Datasets -- ATOMIC-10X
 
 dataset_name = 'ATOMIC10X'
 
-file_template = 'gs://peterw-tpu-eu/standard_data_v1/unannotated_ATOMIC10X_{}.tsv'
+file_template = 'gs://ai2-mosaic-public/projects/symbolic-knowledge-decoding/standard_data_v1/unannotated_ATOMIC10X_{}.tsv'
 input_files = {'train':file_template.format('train'),
             'test':file_template.format('test'),
             'validation':file_template.format('val')}
@@ -153,7 +206,7 @@ Datasets -- ATOMIC2020
 
 dataset_name = 'ATOMIC2020'
 
-file_template = 'gs://peterw-tpu-eu/standard_data_v1/unannotated_ATOMIC2020_{}.tsv'
+file_template = 'gs://ai2-mosaic-public/projects/symbolic-knowledge-decoding/standard_data_v1/unannotated_ATOMIC2020_{}.tsv'
 input_files = {'train':file_template.format('train'),
             'test':file_template.format('test'),
             'validation':file_template.format('val')}
@@ -171,7 +224,7 @@ Datasets -- generated
 
 dataset_name = 'generated'
 
-file_template = 'gs://peterw-tpu-eu/standard_data_v1/unannotated_generated2023_{}.tsv'
+file_template = 'gs://ai2-mosaic-public/projects/symbolic-knowledge-decoding/standard_data_v1/unannotated_generated2023_{}.tsv'
 input_files = {'train':file_template.format('train'),
             'test':file_template.format('test'),
             'validation':file_template.format('val')}
@@ -184,13 +237,19 @@ datasets.append({'input_files': input_files, 'dataset_name':dataset_name,
 
 
 
+
+
 '''
 Datasets -- annotated
+
+The annotated dataset, but also masking every annotation field and never
+masking the generative fields.
+
 '''
 
 dataset_name = 'annotated'
 
-file_template = 'gs://peterw-tpu-eu/standard_data_v1/annotated_{}.tsv'
+file_template = 'gs://ai2-mosaic-public/projects/symbolic-knowledge-decoding/standard_data_v1/annotated_{}.tsv'
 input_files = {'train':file_template.format('train'),
             'test':file_template.format('test'),
             'validation':file_template.format('val')}
@@ -201,6 +260,33 @@ datasets.append({'input_files': input_files, 'dataset_name':dataset_name,
                 'nonempty_fields':nonempty_fields,
                 'share':share})
 
+
+
+'''
+Datasets -- critic
+
+The annotated dataset, but also masking every annotation field and never
+masking the generative fields.
+
+'''
+
+dataset_name = 'critic'
+
+file_template = 'gs://ai2-mosaic-public/projects/symbolic-knowledge-decoding/standard_data_v1/annotated_{}.tsv'
+input_files = {'train':file_template.format('train'),
+            'test':file_template.format('test'),
+            'validation':file_template.format('val')}
+nonempty_fields =  ['premise','hypothesis','question','reasonable','expected','invalid','format','offensive'] 
+share = 1
+dataset = {'input_files': input_files, 'dataset_name':dataset_name,
+                'tsv_fields':tsv_fields,
+                'nonempty_fields':nonempty_fields,
+                'share':share}
+
+task_name = 'train_{}'.format(dataset['dataset_name'])
+build_task(dataset['input_files'], task_name,dataset['tsv_fields'],dataset['nonempty_fields'], field_mask_options=[[0,0,0,1,1,1,1,1]],
+          #metric_fns =[])
+          metric_fns =[metrics.bleu,metrics.rouge])
 
 """
 
@@ -273,7 +359,7 @@ First, generate training tasks
 '''
 for dataset in datasets:
     task_name = 'train_{}'.format(dataset['dataset_name'])
-    build_task(dataset['input_files'], task_name,dataset['tsv_fields'],dataset['nonempty_fields'])
+    build_task(dataset['input_files'], task_name,dataset['tsv_fields'],dataset['nonempty_fields'], metric_fns =[metrics.bleu,metrics.rouge])
     train_tasks.append( (task_name, dataset['share']) )
     
     
