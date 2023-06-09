8e0d7cc471ae5290b04e7d0544716200f65babfc
Peter West <32581513+peterwestuw@users.noreply.github.com>
2023-06-08 17:57:08 -0700

    Update combined_tasks_v2.py

--- t5x/combined_tasks_v2.py
+++ t5x/combined_tasks_v2.py
@@ -1307,7 +1307,7 @@ input_files = {'train':file_template,
             'test':file_template,
             'validation':file_template}
 mask_fields =  ['plausibility']
-build_task(input_files, dataset_name ,mask_fields, metric_fns =[metrics.bleu,metrics.rouge], tsv_fields=['plausibility','query','label','plausibility_p','aux','inference','index','d','context','inputs','prediction'])
+build_task(input_files, dataset_name ,mask_fields, metric_fns =[metrics.bleu,metrics.rouge], tsv_fields=['aux','plausibility','d','inputs','context','plausibility_p','prediction','query','inference','label','index'])
 
 #gs://ai2-mosaic-private/peter-skd-2023/june6_overgenerate_experiment/data_to_score.tsv
 
